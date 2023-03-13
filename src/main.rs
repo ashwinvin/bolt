9d931b7f99440cafcb49ebdb0f58c678fbffb56f
hiro <yirohido@gmail.com>
2023-03-13 04:07:12 +0100

    update UI and add new widgets

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,42 @@
 use iced::widget::button::Button;
 use iced::widget::text_input::TextInput;
-use iced::widget::{button, column, text};
-use iced::{executor, Alignment, Application, Command, Element, Renderer, Settings, Theme};
+use iced::widget::{button, column, container, pick_list, row, text};
+use iced::{executor, Alignment, Application, Command, Element, Length, Renderer, Settings, Theme};
 
 struct BoltState {
     response: String,
     request: String,
+    selected_method: Option<Method>,
 }
 
 #[derive(Debug, Clone)]
 enum Message {
     SendPressed,
     TextInputChanged(String),
+    MethodSelected(Method),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Method {
+    GET,
+    POST,
+}
+
+impl std::fmt::Display for Method {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Method::GET => "GET",
+                Method::POST => "POST",
+            }
+        )
+    }
+}
+
+impl Method {
+    const ALL: [Method; 2] = [Method::GET, Method::POST];
 }
 
 impl Application for BoltState {
@@ -24,6 +49,7 @@ impl Application for BoltState {
         let new_self = Self {
             response: String::from("Response body"),
             request: String::new(),
+            selected_method: Some(Method::GET),
         };
 
         return (new_self, Command::none());
@@ -44,12 +70,23 @@ impl Application for BoltState {
             Message::TextInputChanged(value) => {
                 self.request = value;
             }
+
+            Message::MethodSelected(meth) => {
+                self.selected_method = Some(meth);
+            }
         }
 
         return Command::none();
     }
 
     fn view(&self) -> Element<Message> {
+        let method = pick_list(
+            &Method::ALL[..],
+            self.selected_method,
+            Message::MethodSelected,
+        )
+        .placeholder("METHOD");
+
         let text_box: TextInput<'_, Message, Renderer> =
             TextInput::new("http://", &self.request, Message::TextInputChanged);
 
@@ -57,17 +94,36 @@ impl Application for BoltState {
 
         let response = text(&self.response).size(20);
 
-        let header = text("Bolt API").size(50);
+        let name = row![text("Bolt API").size(30), text("Platform").size(30),];
 
-        let final_view = column![header, text_box, submit, response,]
-            .align_items(Alignment::Center)
-            .into();
+        let extras = row![button("Docs"), button("Settings")];
 
-        return final_view;
+        let header = row![name, extras]
+            .width(Length::Fill)
+            .height(70)
+            .align_items(Alignment::Start);
+
+        let request_panel = row![method, row![text_box].width(500), submit].height(200);
+
+        let response_panel = column![response];
+
+        let editor = column![request_panel, response_panel];
+
+        let sidebar = column![button("API"), button("Collections"), button("Test"),].width(150);
+
+        let body = row![sidebar, editor];
+
+        let final_view: Element<Message> = column![header, body].into();
+
+        return Element::from(
+            container(final_view)
+                .width(Length::Fill)
+                .height(Length::Fill),
+        );
     }
 
     fn theme(&self) -> Self::Theme {
-        Theme::Dark
+        return Theme::Dark;
     }
 }
 
